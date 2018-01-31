417d6eaee2454870c00b419173ad33fff341b3ef
TechnicalFate <c.a.blanchard1997@gmail.com>
2018-01-30 23:05:29 -0600

    I fucked Shit up

---
+++ Test Build - CB/Assets/Scripts/AllyControl.cs
@@ -0,0 +1,26 @@
+﻿// Chris Blanchard
+// Tuesday 1/23/18
+// Controls the movement of Allies when not controlled by the user
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllyControl : MonoBehaviour
+{
+    public Transform followChar; // the character that the ally will follow
+    public float followDist = -2.5f; // Min distance for the player to follow behind
+
+    private float smoothTime = 0.2f; // Time taken to reach followChar
+	
+	void Update ()
+    {
+        transform.LookAt(followChar);
+
+        var velocity = Vector3.zero;
+
+        //Establishes position for Ally to follow behind player
+        Vector3 targetPosition = followChar.TransformPoint(new Vector3(0, 0, followDist));
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+	}
+}
---
+++ Test Build - CB/Assets/Scripts/CharacterHealth.cs
@@ -0,0 +1,53 @@
+﻿// Chris Blanchard
+// Tuesday 1/23/2018
+// Controls the players damage taken and health level
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterHealth : MonoBehaviour
+{
+    public int startHealth = 3;
+    public int currentHealth;
+    public Slider healthSlider;
+
+    bool isDead;
+    bool damaged;
+
+    void Awake()
+    {
+        currentHealth = startHealth;
+        healthSlider.maxValue = startHealth;
+    }
+
+    void Update()
+    {
+        //resets the flag so damage can be taken again
+        damaged = false;
+    }
+
+    public void TakeDamage()
+    {
+        //set flag for damage taken
+        damaged = true;
+
+        currentHealth -= 1;
+
+        healthSlider.value = currentHealth;
+
+        // ensures that when player dies, death method runs
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    // Removes player movement and any other functions related to player
+    void Death()
+    {
+        isDead = true;
+        GetComponent<PlayerController>().enabled = false;
+    }
+}
